5d9a58f54507a162231dd6c07761e1b7f363d8ff
Muneeb Mughal <muneebmughal3324@gmail.com>
2024-10-16 19:41:28 +0500

    exirIfram

--- web/frontend/src/pages/exitIframe/page.tsx
+++ web/frontend/src/pages/exitIframe/page.tsx
@@ -11,12 +11,14 @@ export default function ExitIframe() {
     app.loading(true)
 
     useEffect(() => {
+        console.log('app==>', app)
         if (!!app && !!search) {
             const params = new URLSearchParams(search)
             const redirectUri = params.get('redirectUri')
             const url = new URL(decodeURIComponent(redirectUri!))
 
             if ([location.hostname, 'admin.shopify.com'].includes(url.hostname) || url.hostname.endsWith('.myshopify.com')) {
+                console.log('app bridge first')
                 window.open(url, '_top')
             } else {
                 setShowWarning(true)
